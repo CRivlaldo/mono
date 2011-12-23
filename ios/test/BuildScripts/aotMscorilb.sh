0cec1ad02e1d21ee0115453c676114b68e7109b8
CRivlaldo <crivlaldo@yandex.ru>
2011-12-23 18:08:41 +0400

    mscorlib.dll full-aot build script update

--- ios/test/BuildScripts/aotMscorilb.sh
+++ ios/test/BuildScripts/aotMscorilb.sh
@@ -1,5 +1,8 @@
 readonly DESTINATION_DIR=$(pwd)/../MonoTest/MonoTest/bin/Release
 
+rm -f ${DESTINATION_DIR}/*.o
+rm -f ${DESTINATION_DIR}/mscorlib.dll.a
+
 /Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/arm-apple-darwin-mono --debug --aot=full,static,nodebug,outfile=${DESTINATION_DIR}/mscorlib.dll.o /Developer/Platforms/iPhoneOS.platform/Developer/usr/lib/mono/4.0/mscorlib.dll
 
 ar rcs ${DESTINATION_DIR}/mscorlib.dll.a ${DESTINATION_DIR}/mscorlib.dll.o
